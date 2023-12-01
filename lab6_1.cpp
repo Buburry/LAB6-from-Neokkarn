014f8c9e1612063c5803dfe4591389e00d0cf2ad
Buburry <Natapon7934@gmail.com>
2023-12-01 15:53:55 +0700

    lab6_1.cppcompleted

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,22 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int num=1, Even=0, Odd=0;
+    while (num != 0)
+    {
+        cout << "Enter an integer: ";
+        cin >> num;
+
+        if (num %2 == 0)
+        {
+            Even++;
+        } else {
+            Odd++;
+        }
+        
+    }
+    
+    cout << "#Even numbers = " << Even-1 << endl;
+    cout << "#Odd numbers = " << Odd << endl;
     return 0;
 }
